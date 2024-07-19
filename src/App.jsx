3ca0e61b71b1275e2984f02493d68aa4e6bf7deb
maniraad <maniraad7@gmail.com>
2024-07-19 15:35:50 +0330

    fetch products in table

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <div className="fixed left-0 w-full z-50">
           <Header />
         </div>
-        <div className={`lg:flex-[4] fixed left-0 w-full p-0 ${isSideBarOpen && `xl:w-[85%] p-0 lg:pr-[76px]`} mt-20 transition-all`}>
+        <div className={`lg:flex-[4] absolute left-0 w-full p-0 ${isSideBarOpen && `xl:w-[85%] p-0 lg:pr-[76px]`} mt-20 transition-all`}>
           <div className={`mt-16 px-12 ${isSideBarOpen ? `xl:mr-0` : `xl:mr-20`} transition-all`}>
             {router}
           </div>
--- src/Components/Comments/ProductsTable/ProductsTable.jsx
+++ src/Components/Comments/ProductsTable/ProductsTable.jsx
@@ -6,6 +6,7 @@ export default function ProductsTable() {
 
     const [isShowModal, setIsShowModal] = useState(false)
     const [isShowEditModal, setIsShowEditModal] = useState(false)
+    const [allProducts, setAllProducts] = useState([])
 
     const DeleteModalCancelAction = () => setIsShowModal(false)
     const DeleteModalSubmitAction = () => {
@@ -15,6 +16,12 @@ export default function ProductsTable() {
     const updateProductInfos = () => {
         console.log('محصول ویرایش شد');
     }
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/products/")
+            .then(res => res.json())
+            .then(products => setAllProducts(products))
+    }, [])
 
 
     return (
@@ -41,137 +48,57 @@ export default function ProductsTable() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td className="p-4">
-                                <img src="/docs/images/products/apple-watch.png" className="w-16 md:w-32 max-w-full max-h-full" alt="Apple Watch" />
-                            </td>
-                            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                Apple Watch
-                            </td>
-                            <td className="px-6 py-4">
-                                <div className="flex items-center">
-                                    <button className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                        <span className="sr-only">Quantity button</span>
-                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
-                                        </svg>
-                                    </button>
-                                    <div>
-                                        <input type="number" id="first_product" className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required />
-                                    </div>
-                                    <button className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                        <span className="sr-only">Quantity button</span>
-                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                $599
-                            </td>
-                            <td className="px-6 py-4 child:mx-1.5 max-w-[120px]">
-                                <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">جزئیات</button>
-                                <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">حذف</button>
-                                <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">ویرایش</button>
-                            </td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td className="p-4">
-                                <img src="/docs/images/products/imac.png" className="w-16 md:w-32 max-w-full max-h-full" alt="Apple iMac" />
-                            </td>
-                            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                iMac 27"
-                            </td>
-                            <td className="px-6 py-4">
-                                <div className="flex items-center">
-                                    <button className="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                        <span className="sr-only">Quantity button</span>
-                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
-                                        </svg>
-                                    </button>
-                                    <div className="ms-3">
-                                        <input type="number" id="first_product" className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required />
-                                    </div>
-                                    <button className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                        <span className="sr-only">Quantity button</span>
-                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                $2499
-                            </td>
-                            <td className="px-6 py-4 child:mx-1.5 max-w-[120px]">
-                                <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">جزئیات</button>
-                                <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">حذف</button>
-                                <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">ویرایش</button>
-                            </td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td className="p-4">
-                                <img src="/docs/images/products/iphone-12.png" className="w-16 md:w-32 max-w-full max-h-full" alt="iPhone 12" />
-                            </td>
-                            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                IPhone 12
-                            </td>
-                            <td className="px-6 py-4">
-                                <div className="flex items-center">
-                                    <button className="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                        <span className="sr-only">Quantity button</span>
-                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
-                                        </svg>
-                                    </button>
-                                    <div className="ms-3">
-                                        <input type="number" id="first_product" className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required />
-                                    </div>
-                                    <button className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                        <span className="sr-only">Quantity button</span>
-                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                $999
-                            </td>
-                            <td className="px-6 py-4 child:mx-1.5 max-w-[120px]">
-                                <button type="button" onClick={() => { setIsShowModal(true) }} className="px-3 py-2 font-Estedad300 text-sm text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none">حذف</button>
-                                <button type="button" onClick={() => setIsShowEditModal(true)} className="px-3 py-2 font-Estedad300 text-sm text-center text-white bg-[#4880FF] hover:bg-[#416dd3] rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300">ویرایش</button>
-                            </td>
-                        </tr>
+
+                        {allProducts.map(product => (
+                            
+                            
+                            <tr key={product.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                               {console.log(product)}
+                                <td className="p-4">
+                                    <img src={product.img} className="w-16 md:w-32 max-w-full max-h-full" alt={product.title} />
+                                </td>
+                                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                                    {product.title}
+                                </td>
+                                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                                {product.price } تومان 
+                                </td>
+                                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                                {product.count } 
+                                </td>
+                                <td className="px-6 py-4 child:mx-1.5 max-w-[120px]">
+                                    <button type="button" onClick={() => { setIsShowModal(true) }} className="px-3 py-2 font-Estedad300 text-sm text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none">حذف</button>
+                                    <button type="button" onClick={() => setIsShowEditModal(true)} className="px-3 py-2 font-Estedad300 text-sm text-center text-white bg-[#4880FF] hover:bg-[#416dd3] rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300">ویرایش</button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
 
             {isShowModal && <DeleteModal DeleteModalCancelAction={DeleteModalCancelAction} DeleteModalSubmitAction={DeleteModalSubmitAction} />}
             {isShowEditModal &&
-            <EditModal
-                onClose={() => setIsShowEditModal(false)}
-                onSubmit={updateProductInfos}
-            >
-                <div>
-                    <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
-                </div>
-                <div>
-                    <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
-                </div>
-                <div>
-                    <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
-                </div>
-                <div>
-                    <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
-                </div>
-            </EditModal>}
+                <EditModal
+                    onClose={() => setIsShowEditModal(false)}
+                    onSubmit={updateProductInfos}
+                >
+                    <div>
+                        <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
+                    </div>
+                    <div>
+                        <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
+                    </div>
+                    <div>
+                        <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
+                    </div>
+                    <div>
+                        <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">عنوان محصول</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="عنوان جدید را وارد کنید" required />
+                    </div>
+                </EditModal>}
 
         </>
     )
